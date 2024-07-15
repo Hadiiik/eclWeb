d08799b0ddc4d20ac2d722cba39af6a1445346dd
hadiikaddour <hadyqdwr77@gmail.com>
2024-07-15 22:56:49 +0300

    fixing stuff

--- app/api/fiels/upload/route.ts
+++ app/api/fiels/upload/route.ts
@@ -2,7 +2,6 @@ import { supabase } from "@/lib/supabase";
 import { rateLimiterMiddleware } from "@/middleware/rateLimiterMiddleware";
 import { NextRequest, NextResponse } from "next/server";
 
-export const responseLimit = "20mb";
 
 export async function POST(req: NextRequest) {
     const rateLimitResponse = await rateLimiterMiddleware(req);
